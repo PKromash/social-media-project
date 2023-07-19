758c48e7068148f4d10f063fe33b4f798e9c1df2
Peyton Kromash <pkromash@gmail.com>
2023-07-19 15:02:36 -0400

    fixed error 2

--- components/Provider.js
+++ components/Provider.js
@@ -4,11 +4,11 @@ import {SessionProvider} from "next-auth/react";
 import thunk from "redux-thunk";
 import {Provider} from "react-redux";
 import {configureStore} from "@reduxjs/toolkit";
-import reducers from "../redux/reducers/index";
+import Reducers from "../redux/reducers/index";
 import {ChakraProvider} from "@chakra-ui/react";
 
 const store = configureStore({
-  reducer: reducers,
+  reducer: Reducers,
   middleware: [thunk],
 });
 

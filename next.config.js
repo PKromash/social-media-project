029fda05bfedf092016ac0147471a03b5dda97da
Peyton Kromash <pkromash@gmail.com>
2023-07-23 11:42:09 -0400

    changed next-config file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  headers: {
-    "Cache-Control": "no-store",
-  },
   images: {
     domains: ["lh3.googleusercontent.com"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Match all routes
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
+        ],
+      },
+    ];
   },
 };
 

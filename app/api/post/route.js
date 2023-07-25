ee59ce80d65c43000ed82838d0099754bd50685e
Peyton Kromash <pkromash@gmail.com>
2023-07-25 18:46:29 -0400

    added debugging statements again

--- app/api/post/route.js
+++ app/api/post/route.js
@@ -3,8 +3,10 @@ import connectToDB from "@/database";
 
 export const GET = async (request) => {
   try {
+    console.log("Connecting to the database...");
     await connectToDB();
 
+    console.log("Fetching all posts...");
     const posts = await Post.find({})
       .populate("creator")
       .populate({
@@ -14,8 +16,10 @@ export const GET = async (request) => {
         },
       });
 
+    console.log("Posts fetched successfully.");
     return new Response(JSON.stringify(posts), {status: 200});
   } catch (error) {
+    console.error("Error fetching all posts:", error);
     return new Response("Failed to fetch all posts", {status: 500});
   }
 };
